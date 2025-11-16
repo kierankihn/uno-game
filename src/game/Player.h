375abeb2bbf21e4da701d4d4ab32b56eba594695
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2025-11-16 22:06:42 +0800

    fix(game): update `Player` constructor to take `std::string` by value instead of reference

--- src/game/Player.h
+++ src/game/Player.h
@@ -66,7 +66,7 @@ namespace UNO::GAME {
     public:
         HandCard *handCard;
 
-        explicit Player(std::string &name);
+        explicit Player(std::string name);
 
         /**
          * @return 返回玩家名字
