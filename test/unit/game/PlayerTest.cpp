e5f9666ff072eab4e943c9a8bb3fada71493cb94
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2025-11-17 21:17:37 +0800

    test(game): refactor `PlayerTest` to use `HandCard::draw` method instead of array constructor

--- test/unit/game/PlayerTest.cpp
+++ test/unit/game/PlayerTest.cpp
@@ -5,22 +5,22 @@
  * @date 2025.11.16
  */
 
-#include "../../../src/game/Card.h"
 #include "../../../src/game/Player.h"
+#include "../../../src/game/Card.h"
 
 #include <gtest/gtest.h>
 
 TEST(player_test, player_test_1)
 {
-    UNO::GAME::HandCard handCard(std::array<UNO::GAME::Card, 7>{
-        UNO::GAME::Card(UNO::GAME::CardColor::BLUE, UNO::GAME::CardType::REVERSE),
-        UNO::GAME::Card(UNO::GAME::CardColor::RED, UNO::GAME::CardType::SKIP),
-        UNO::GAME::Card(UNO::GAME::CardColor::BLUE, UNO::GAME::CardType::NUM3),
-        UNO::GAME::Card(UNO::GAME::CardColor::RED, UNO::GAME::CardType::WILDDRAWFOUR),
-        UNO::GAME::Card(UNO::GAME::CardColor::GREEN, UNO::GAME::CardType::NUM9),
-        UNO::GAME::Card(UNO::GAME::CardColor::RED, UNO::GAME::CardType::NUM1),
-        UNO::GAME::Card(UNO::GAME::CardColor::RED, UNO::GAME::CardType::WILD)
-    });
+    UNO::GAME::HandCard handCard;
+
+    handCard.draw(UNO::GAME::Card(UNO::GAME::CardColor::BLUE, UNO::GAME::CardType::REVERSE));
+    handCard.draw(UNO::GAME::Card(UNO::GAME::CardColor::RED, UNO::GAME::CardType::SKIP));
+    handCard.draw(UNO::GAME::Card(UNO::GAME::CardColor::BLUE, UNO::GAME::CardType::NUM3));
+    handCard.draw(UNO::GAME::Card(UNO::GAME::CardColor::RED, UNO::GAME::CardType::WILDDRAWFOUR));
+    handCard.draw(UNO::GAME::Card(UNO::GAME::CardColor::GREEN, UNO::GAME::CardType::NUM9));
+    handCard.draw(UNO::GAME::Card(UNO::GAME::CardColor::RED, UNO::GAME::CardType::NUM1));
+    handCard.draw(UNO::GAME::Card(UNO::GAME::CardColor::RED, UNO::GAME::CardType::WILD));
 
     ASSERT_EQ(handCard.getCards().begin()->getColor(), UNO::GAME::CardColor::RED);
     ASSERT_EQ(handCard.getCards().begin()->getType(), UNO::GAME::CardType::NUM1);
@@ -36,5 +36,4 @@ TEST(player_test, player_test_1)
     ASSERT_EQ(handCard.getCards().begin()->getType(), UNO::GAME::CardType::NUM0);
 
     ASSERT_EQ(handCard.isEmpty(), false);
-
 }
